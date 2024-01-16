07389acc055c1c683bf53db70c9c917114bcf64b
Your Name <github1504@gmail.com>
2024-01-16 17:07:27 +0530

    refactoring

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -166,8 +166,6 @@ export const loginUser = CatchAsyncError(
 
       const user = await userModel.findOne({ email }).select("+password");
 
-      console.log(user);
-
       if (!user) {
         return next(new ErrorHandler("Invalid email or password.", 400));
       }
